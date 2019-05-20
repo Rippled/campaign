d4ef86db5df75228388612b6e8c8a9b20cc14ebc
Lincoln White <notforsale@gmx.com>
2019-05-20 19:07:45 +0100

    Created file

---
+++ CampaignUser.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary
+{
+    public class CampaignUser
+    {
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public DateTime LastUpdated { get; set; }
+    }
+}
